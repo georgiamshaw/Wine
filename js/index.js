b17011b365f968d4e0f74ff03245b980070bebca
georgiashaw <georgiamshaw@yahoo.co.uk>
2018-03-27 22:11:36 +0100

    sorting out indenting

--- js/index.js
+++ js/index.js
@@ -1,14 +1,9 @@
 
-  /* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
